30f8def115a11c414d98b8fe96e3c1f421b40d24
Miles Johnson <mileswjohnson@gmail.com>
2011-07-28 22:15:28 -0700

    Removing DecodaConfig and placing logic in each hook.

--- hooks/CensorHook.php
+++ hooks/CensorHook.php
@@ -1,1 +1,27 @@
 <?php
+
+class CensorHook extends DecodaHook {
+	
+    /**
+     * List of words to censor.
+     *
+     * @access protected
+     * @var array
+     */
+    protected $_censored = array();
+	
+    /**
+     * Load the censored words from the text file.
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct() {
+		$path = DECODA_CONFIG .'censored.txt';
+
+		if (file_exists($path)) {
+			$this->_censored = array_map('trim', file($path));
+		}
+    }
+	
+}
--- hooks/EmoticonHook.php
+++ hooks/EmoticonHook.php
@@ -1,1 +1,27 @@
 <?php
+
+class EmoticonHook extends DecodaHook {
+	
+    /**
+     * Mapping of emoticons and smilies.
+     *
+     * @access protected
+     * @var array
+     */
+    protected $_emoticons = array();
+	
+    /**
+     * Load the emoticons from the JSON file.
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct() {
+		$path = DECODA_CONFIG .'emoticons.json';
+
+		if (file_exists($path)) {
+			$this->_emoticons = json_decode(file_get_contents($path), true);
+		}
+    }
+	
+}

db6c130c9e890fe74d97db77aa584a0bdd8886cb
Miles Johnson <mileswjohnson@gmail.com>
2012-12-07 16:52:33 -0800

    Made video URLs protocol agnostic [Issue #29]

--- filters/VideoFilter.php
+++ filters/VideoFilter.php
@@ -126,56 +126,56 @@ class VideoFilter extends FilterAbstract {
 			'medium' => array(640, 360),
 			'large' => array(853, 480),
 			'player' => 'iframe',
-			'path' => 'http://youtube.com/embed/{id}'
+			'path' => '//youtube.com/embed/{id}'
 		),
 		'vimeo' => array(
 			'small' => array(400, 225),
 			'medium' => array(550, 309),
 			'large' => array(700, 394),
 			'player' => 'iframe',
-			'path' => 'http://player.vimeo.com/video/{id}'
+			'path' => '//player.vimeo.com/video/{id}'
 		),
 		'liveleak' => array(
 			'small' => array(560, 315),
 			'medium' => array(640, 360),
 			'large' => array(853, 480),
 			'player' => 'iframe',
-			'path' => 'http://liveleak.com/e/{id}'
+			'path' => '//liveleak.com/e/{id}'
 		),
 		'veoh' => array(
 			'small' => array(410, 341),
 			'medium' => array(610, 507),
 			'large' => array(810, 674),
 			'player' => 'embed',
-			'path' => 'http://veoh.com/swf/webplayer/WebPlayer.swf?version=AFrontend.5.7.0.1390&permalinkId={id}&player=videodetailsembedded&videoAutoPlay=0&id=anonymous'
+			'path' => '//veoh.com/swf/webplayer/WebPlayer.swf?version=AFrontend.5.7.0.1390&permalinkId={id}&player=videodetailsembedded&videoAutoPlay=0&id=anonymous'
 		),
 		'dailymotion' => array(
 			'small' => array(320, 180),
 			'medium' => array(480, 270),
 			'large' => array(560, 315),
 			'player' => 'iframe',
-			'path' => 'http://dailymotion.com/embed/video/{id}'
+			'path' => '//dailymotion.com/embed/video/{id}'
 		),
 		'myspace' => array(
 			'small' => array(325, 260),
 			'medium' => array(425, 340),
 			'large' => array(525, 420),
 			'player' => 'embed',
-			'path' => 'http://mediaservices.myspace.com/services/media/embed.aspx/m={id},t=1,mt=video'
+			'path' => '//mediaservices.myspace.com/services/media/embed.aspx/m={id},t=1,mt=video'
 		),
 		'wegame' => array(
 			'small' => array(325, 223),
 			'medium' => array(480, 330),
 			'large' => array(640, 440),
 			'player' => 'embed',
-			'path' => 'http://wegame.com/static/flash/player.swf?xmlrequest=http://www.wegame.com/player/video/{id}&embedPlayer=true'
+			'path' => '//wegame.com/static/flash/player.swf?xmlrequest=http://www.wegame.com/player/video/{id}&embedPlayer=true'
 		),
 		'collegehumor' => array(
 			'small' => array(300, 169),
 			'medium' => array(450, 254),
 			'large' => array(600, 338),
 			'player' => 'iframe',
-			'path' => 'http://collegehumor.com/e/{id}'
+			'path' => '//collegehumor.com/e/{id}'
 		)
 	);
 

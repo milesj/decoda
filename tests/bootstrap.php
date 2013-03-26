fca8cbe359cfb77fab09bc8a0907a6bfce7e55c6
Carsten Blüm <carsten@bluem.net>
2013-03-26 19:53:42 +0100

    Removed unnecessary realpath() call

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL | E_STRICT);
 
 // Set constants
 define('TEST_DIR', __DIR__);
-define('VENDOR_DIR', realpath(__DIR__ . '/../../../../vendor'));
+define('VENDOR_DIR', __DIR__ . '/../../../../vendor');
 define('DECODA', dirname(TEST_DIR) . DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Decoda/');
 
 // Ensure that composer has installed all dependencies

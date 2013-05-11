7949f14a1ebc5c0228f4103b3b2bcabdb33aab63
alquerci <alquerci@email.com>
2013-05-12 01:47:36 +0200

    Refactored tests for the CensorHook class
    Try to make more accurate tests.
    The equivalent censored contents and number of letters are tested.

--- tests/Decoda/Hook/CensorHookTest.php
+++ tests/Decoda/Hook/CensorHookTest.php
@@ -13,6 +13,8 @@ use Decoda\Test\TestCase;
 
 class CensorHookTest extends TestCase {
 
+    protected $equivalentChars = '@#$*!?%';
+
     /**
      * Set up Decoda.
      */
@@ -28,15 +30,15 @@ class CensorHookTest extends TestCase {
      * Test that beforeParse() will convert curse words to a censored equivalent. Will also take into account mulitple characters.
      */
     public function testParse() {
-        $this->assertNotEquals('fuck', $this->object->beforeParse('fuck'));
-        $this->assertNotEquals('fuck fuckk fucckk', $this->object->beforeParse('fuck fuckk fucckk'));
-        $this->assertNotEquals('fuuuccckkkkk', $this->object->beforeParse('fuuuccckkkkk'));
-        $this->assertNotEquals('fffUUUcccKKKkk', $this->object->beforeParse('fffUUUcccKKKkk'));
-        $this->assertNotEquals('Hey, fuck you buddy!', $this->object->beforeParse('Hey, fuck you buddy!'));
+        $this->assertRegExp(sprintf('/%s/', $this->getCensoredRegex(4)), $this->object->beforeParse('fuck'));
+        $this->assertRegExp(sprintf('/%s/', $this->getCensoredRegex(12)), $this->object->beforeParse('fuuuccckkkkk'));
+        $this->assertRegExp(sprintf('/%s/', $this->getCensoredRegex(14)), $this->object->beforeParse('fffUUUcccKKKkk'));
+        $this->assertRegExp(sprintf('/%s %s %s/', $this->getCensoredRegex(4), $this->getCensoredRegex(5), $this->getCensoredRegex(6)), $this->object->beforeParse('fuck fuckk fucckk'));
+        $this->assertRegExp(sprintf('/Hey, %s you buddy!/', $this->getCensoredRegex(4)), $this->object->beforeParse('Hey, fuck you buddy!'));
 
         // Don't censor words that share a blacklist
-        $this->assertNotEquals('nig', $this->object->beforeParse('nig'));
-        $this->assertNotEquals('nigger', $this->object->beforeParse('nigger'));
+        $this->assertRegExp(sprintf('/%s/', $this->getCensoredRegex(3)), $this->object->beforeParse('nig'));
+        $this->assertRegExp(sprintf('/%s/', $this->getCensoredRegex(6)), $this->object->beforeParse('nigger'));
         $this->assertEquals('Night', $this->object->beforeParse('Night'));
     }
 
@@ -47,8 +49,26 @@ class CensorHookTest extends TestCase {
         $this->assertEquals('word', $this->object->beforeParse('word'));
 
         $this->object->blacklist(array('word'));
-        $this->assertNotEquals('word', $this->object->beforeParse('word'));
-        $this->assertNotEquals('wooRrrDdd', $this->object->beforeParse('wooRrrDdd'));
+        $this->assertRegExp(sprintf('/%s/', $this->getCensoredRegex(4)), $this->object->beforeParse('word'));
+        $this->assertRegExp(sprintf('/%s/', $this->getCensoredRegex(9)), $this->object->beforeParse('wooRrrDdd'));
+    }
+
+    /**
+     * Gets the censored equivalent regex with the specified length
+     *
+     * @param integer $length The number of characters for the equivalent regex
+     * @return string The censored equivalent regex with a maximum of 10 letters
+     */
+    protected function getCensoredRegex($length)
+    {
+        // The string censored equivalent is limited to 10 letters
+        if (10 < $length) {
+            $length = 10;
+        }
+
+        $regex = sprintf('[%s]{%d}', preg_quote($this->equivalentChars, '/'), $length);
+
+        return $regex;
     }
 
 }

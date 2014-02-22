250b7964d470f6de5103d090f0b66c1bba43a365
Miles Johnson <mileswjohnson@gmail.com>
2014-02-22 12:09:37 -0800

    Update copyright years

--- src/Decoda/Filter/EmptyFilter.php
+++ src/Decoda/Filter/EmptyFilter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright   2006-2013, Miles Johnson - http://milesj.me
+ * @copyright   2006-2014, Miles Johnson - http://milesj.me
  * @license     https://github.com/milesj/decoda/blob/master/license.md
  * @link        http://milesj.me/code/php/decoda
  */

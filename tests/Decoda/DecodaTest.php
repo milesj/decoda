2b537430830a26956b5e58c269c42612554a9816
alquerci <alquerci@email.com>
2014-08-03 08:09:47 +0200

    Fixed test for unix platform
    Unix directory separator is not a backslash instead use `DIRECTORY_SEPARATOR`.

--- tests/Decoda/DecodaTest.php
+++ tests/Decoda/DecodaTest.php
@@ -136,7 +136,7 @@ class DecodaTest extends TestCase {
         $this->object->addPath('/some/folder')->addPath('/another/folder/');
 
         $this->assertEquals(array(
-            dirname(dirname(__DIR__)) . '\src\config/',
+            dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'config/',
             '/some/folder/',
             '/another/folder/'
         ), $this->object->getPaths());
